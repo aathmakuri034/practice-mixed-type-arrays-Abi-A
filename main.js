349eac0fbbe6ef12c0ae1d297c6a5f33c50030df
Abi Athmakuri <aathmakuri034@gmail.com>
2026-04-27 10:35:50 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let profile = ["Alice", 13, true, "St.Louis", ["Tennis", "Building Projects"]]
+
+console.log(profile[0]);
+console.log(profile[4][1]);
+
+profile[1] = 16;
+profile[4].push("Swimming");
+console.log(profile);
